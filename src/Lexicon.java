7e08fd7f54f7d83b438f9ad990b28610eac7703d
nurelmamasharipov <nurikmathematic@gmail.com>
2024-08-02 13:20:51 +0600

    Initial commit

---
+++ src/Lexicon.java
@@ -0,0 +1,14 @@
+public class Lexicon {
+
+    private int lexicon;
+
+    public Lexicon(int lexicon) {
+        this.lexicon = lexicon;
+    }
+
+    public int getLexicon() {
+        return lexicon;
+    }
+
+
+}
